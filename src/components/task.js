0b76d07e100e0a8ab10035ffe10253c4bafb7ddf
Alex Kapin <alexander.kapin@outlook.com>
2020-04-09 13:11:09 +0300

    hide task date markup if there is no date

--- src/components/task.js
+++ src/components/task.js
@@ -37,14 +37,15 @@ export const createTaskTemplate = (task) => {
         </div>
         <div class="card__settings">
           <div class="card__details">
-            <div class="card__dates">
-              <div class="card__date-deadline">
-                <p class="card__input-deadline-wrap">
-                  <span class="card__date">${date}</span>
-                  <span class="card__time">${time}</span>
-                </p>
-              </div>
-            </div>
+          ${ isDateShowing ?
+    `<div class="card__dates">
+      <div class="card__date-deadline">
+        <p class="card__input-deadline-wrap">
+          <span class="card__date">${date}</span>
+          <span class="card__time">${time}</span>
+        </p>
+      </div>
+    </div>` : ``}
           </div>
         </div>
       </div>

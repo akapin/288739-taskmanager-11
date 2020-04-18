24efdba569fcdd90d6a0014c7e3a00488224097c
Alexander Kapin <alexander.kapin@gmail.com>
2020-04-18 09:49:08 +0300

    5.5.3 Перенесет рендеринг  в функцию. В обработчике изменения типа сортировки отрендерим задачи не сортируя их

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -55,6 +55,26 @@ export default class BoardController {
   }
 
   render(tasks) {
+    const renderLoadMoreButton = () => {
+      if (showingTasksCount >= tasks.length) {
+        return;
+      }
+
+      render(container, this._loadMoreButtonComponent);
+
+      this._loadMoreButtonComponent.setClickHandler(() => {
+        const prevTasksCount = showingTasksCount;
+        showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+        tasks.slice(prevTasksCount, showingTasksCount)
+          .forEach((task) => renderTask(taskListElement, task));
+
+        if (showingTasksCount >= tasks.length) {
+          remove(this._loadMoreButtonComponent);
+        }
+      });
+    };
+
     const container = this._container.getElement();
     const isAllTasksArchived = tasks.every((task) => task.isArchive);
 
@@ -74,22 +94,19 @@ export default class BoardController {
         renderTask(taskListElement, task);
       });
 
-    render(container, this._loadMoreButtonComponent);
-
-    this._loadMoreButtonComponent.setClickHandler(() => {
-      const prevTasksCount = showingTasksCount;
-      showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-      tasks.slice(prevTasksCount, showingTasksCount)
-        .forEach((task) => renderTask(taskListElement, task));
-
-      if (showingTasksCount >= tasks.length) {
-        remove(this._loadMoreButtonComponent);
-      }
-    });
+    renderLoadMoreButton();
 
     this._sortComponent.setSortTypeChangeHandler(() => {
+      showingTasksCount = SHOWING_TASKS_COUNT_BY_BUTTON;
 
+      taskListElement.innerHTML = ``;
+
+      tasks.slice(0, showingTasksCount)
+        .forEach((task) => {
+          renderTask(taskListElement, task);
+        });
+
+      renderLoadMoreButton();
     });
   }
 }

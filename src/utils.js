8fb745a71a829e2466099bf9941d2500a0e46dfe
Alexander Kapin <alexander.kapin@gmail.com>
2020-04-18 09:11:12 +0300

    5.2.1 Перенесет функции для рендеринга из  в

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,5 @@
 import {MONTH_NAMES} from "./const.js";
 
-export const RenderPosition = {
-  AFTERBEGIN: `afterbegin`,
-  BEFOREEND: `beforeend`
-};
-
 const castTimeFormat = (value) => {
   return value < 10 ? `0${value}` : String(value);
 };
@@ -22,25 +17,4 @@ export const getTaskDateProperties = (dueDate) => {
   const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
   const time = isDateShowing ? formatTime(dueDate) : ``;
   return {isExpired, isDateShowing, date, time};
-};
-
-export const createElement = (template) => {
-  const newElement = document.createElement(`div`);
-  newElement.innerHTML = template;
-
-  return newElement.firstChild;
-};
-
-export const render = (container, element, place = RenderPosition.BEFOREEND) => {
-  switch (place) {
-    case RenderPosition.AFTERBEGIN:
-      container.prepend(element);
-      break;
-    case RenderPosition.BEFOREEND:
-      container.append(element);
-      break;
-    default:
-      container.append(element);
-      break;
-  }
 };

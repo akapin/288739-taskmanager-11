a954c542630a44a625b9cc55240251063dabebdf
Alex Kapin <alexander.kapin@outlook.com>
2020-04-22 13:58:12 +0300

    6.1.6 Добавит обработчики на кнопки и подписку на событие. Оптимизирует отрисовку кнопок

--- src/components/task.js
+++ src/components/task.js
@@ -1,57 +1,65 @@
 import AbstractComponent from "./abstract-component.js";
 import {getTaskDateProperties} from "../utils/common.js";
 
+const createButtonMarkup = (name, isActive = true) => {
+  return (
+    `<button
+      type="button"
+      class="card__btn card__btn--${name} ${isActive ? `` : `card__btn--disabled`}"
+    >
+      ${name}
+    </button>`
+  );
+};
+
 const createTaskTemplate = (task) => {
-  const {description, dueDate, color, repeatingDays, isArchive, isFavorite} = task;
+  const {description, dueDate, color, repeatingDays} = task;
 
   const {isExpired, isDateShowing, date, time} = getTaskDateProperties(dueDate);
 
+  const editButton = createButtonMarkup(`edit`);
+  const archiveButton = createButtonMarkup(`archive`, !task.isArchive);
+  const favoritesButton = createButtonMarkup(`favorites`, !task.isFavorite);
+
   const repeatClass = Object.values(repeatingDays).some(Boolean) ? `card--repeat` : ``;
   const deadlineClass = isExpired ? `card--deadline` : ``;
-  const archiveButtonInactiveClass = isArchive ? `` : `card__btn--disabled`;
-  const favoriteButtonInactiveClass = isFavorite ? `` : `card__btn--disabled`;
 
-  return `<article class="card card--${color} ${repeatClass} ${deadlineClass}">
-    <div class="card__form">
-      <div class="card__inner">
-        <div class="card__control">
-          <button type="button" class="card__btn card__btn--edit">
-            edit
-          </button>
-          <button type="button" class="card__btn card__btn--archive ${archiveButtonInactiveClass}">
-            archive
-          </button>
-          <button
-            type="button"
-            class="card__btn card__btn--favorites ${favoriteButtonInactiveClass}"
-          >
-            favorites
-          </button>
+  return (
+    `<article class="card card--${color} ${repeatClass} ${deadlineClass}">
+      <div class="card__form">
+        <div class="card__inner">
+          <div class="card__control">
+            ${editButton}
+            ${archiveButton}
+            ${favoritesButton}
+          </div>
+
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <p class="card__text">${description}</p>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+            ${ isDateShowing ?
+      `<div class="card__dates">
+        <div class="card__date-deadline">
+          <p class="card__input-deadline-wrap">
+            <span class="card__date">${date}</span>
+            <span class="card__time">${time}</span>
+          </p>
         </div>
-        <div class="card__color-bar">
-          <svg class="card__color-bar-wave" width="100%" height="10">
-            <use xlink:href="#wave"></use>
-          </svg>
-        </div>
-        <div class="card__textarea-wrap">
-          <p class="card__text">${description}</p>
-        </div>
-        <div class="card__settings">
-          <div class="card__details">
-          ${ isDateShowing ?
-    `<div class="card__dates">
-      <div class="card__date-deadline">
-        <p class="card__input-deadline-wrap">
-          <span class="card__date">${date}</span>
-          <span class="card__time">${time}</span>
-        </p>
-      </div>
-    </div>` : ``}
+      </div>` : ``}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </article>`;
+    </article>`);
 };
 
 export default class Task extends AbstractComponent {
@@ -63,6 +71,16 @@ export default class Task extends AbstractComponent {
 
   getTemplate() {
     return createTaskTemplate(this._task);
+  }
+
+  setFavoritesButtonClickHandler(handler) {
+    this.getElement().querySelector(`.card__btn--favorites`)
+      .addEventListener(`click`, handler);
+  }
+
+  setArchiveButtonClickHandler(handler) {
+    this.getElement().querySelector(`.card__btn--archive`)
+      .addEventListener(`click`, handler);
   }
 
   setEditButtonClickHandler(handler) {
